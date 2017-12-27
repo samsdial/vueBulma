a7c5761dad1c5c4095ce51af7d7a3641ca54e1fe
samsdial <dialstudios@gmail.com>
2017-12-27 13:47:52 -0500

    Add Vue reosurce Fin

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,2 +1,2 @@
-"use strict";var vm=new Vue({el:"main",data:{nuevaTarea:null},methods:{},computed:{}});
+"use strict";var vm=new Vue({el:"main",mounted:function(){this.cargarPesonas()},data:{personas:[]},methods:{cargarPesonas:function(){var s=this;this.$http.get("https://randomuser.me/api/?results=50").then(function(t){s.personas=t.body.results})}}});
 //# sourceMappingURL=custom.js.map
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,13 +1,18 @@
 
 const vm = new Vue ({
     el: 'main',
+    mounted(){
+        this.cargarPesonas();
+    },
     data: {
-        nuevaTarea: null,
+        personas: [],
     },
-    methods:{
-
-    },
-    computed: {
-
+    methods: {
+        cargarPesonas(){
+            this.$http.get('https://randomuser.me/api/?results=50')
+                .then((respuesta) =>{
+                    this.personas = respuesta.body.results;
+                });
+        }
     }
 });
