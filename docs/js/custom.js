107fa6a82c0ad415c05e4f62bbfe12e5d5287ddc
samsdial <dialstudios@gmail.com>
2017-12-27 17:51:16 -0500

    Add Video Component templates Fin

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,2 +1,2 @@
-"use strict";var vm=new Vue({el:"main",data:{nuevaTarea:null},methods:{},computed:{}});
+"use strict";Vue.component("elegir-ganador",{props:["listado"],methods:{elegirGanador:function(){var a=this.participantes.length,t=Math.floor(Math.random()*a);this.ganador=this.participantes[t-1]}},data:function(){return{ganador:!1,participantes:this.listado}}});var vm=new Vue({el:"main",data:{personas:["juan","alicia","pedro","javier"]}});
 //# sourceMappingURL=custom.js.map
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,13 +1,27 @@
-
+Vue.component('elegir-ganador',{
+    props: ['listado'],
+    methods:{
+        elegirGanador(){
+            let cantidad = this.participantes.length;
+            let indice = Math.floor((Math.random() * cantidad));
+            this.ganador = this.participantes[indice - 1];
+        }
+    },
+    data() {
+        return {
+            ganador: false,
+            participantes: this.listado
+        }
+    },
+});
 const vm = new Vue ({
     el: 'main',
     data: {
-        nuevaTarea: null,
-    },
-    methods:{
-
-    },
-    computed: {
-
+        personas: [
+            'juan',
+            'alicia',
+            'pedro',
+            'javier'
+        ]
     }
 });
