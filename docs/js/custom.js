9647854dbbc019fc499a86dbad1067790f21afa5
samsdial <dialstudios@gmail.com>
2017-12-27 14:30:31 -0500

    Add Vue Axios Fin Cusro

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,2 +1,2 @@
-"use strict";var vm=new Vue({el:"main",data:{nuevaTarea:null},methods:{},computed:{}});
+"use strict";var vm=new Vue({el:"main",mounted:function(){this.cargarPersonas()},data:{personas:[]},methods:{cargarPersonas:function(){var s=this;axios.get("https://randomuser.me/api/?results=5000").then(function(a){s.personas=a.data.results})}}});
 //# sourceMappingURL=custom.js.map
--- src/js/custom.js
+++ src/js/custom.js
@@ -1,13 +1,20 @@
 
 const vm = new Vue ({
     el: 'main',
+    mounted(){
+      this.cargarPersonas();
+    },
     data: {
-        nuevaTarea: null,
+        personas: []
     },
     methods:{
-
-    },
-    computed: {
-
+        cargarPersonas(){
+            //console.log('vue');
+            axios.get('https://randomuser.me/api/?results=5000')
+                .then((respuesta) =>{
+                    //console.log(respuesta);
+                    this.personas = respuesta.data.results;
+                });
+        }
     }
 });
